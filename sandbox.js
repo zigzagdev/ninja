117c26a3fb926afc2878b0b17b9ba5386edfcf05
Masatoshi <msts.oo0131@gmail.com>
2021-08-27 00:08:55 +0900

    array_any_learned

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,22 @@
-
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var names = ['luigi', 'mario', 'peach'];
+names.push('toad');
+var numbers = [10, 20, 40];
+numbers.push(66);
+var mixed = ['ken', 3, 'test', 6];
+mixed.push('ronaldo');
+mixed[0] = 4;
+// union types
+var ronaldo = [];
+ronaldo.push('fuck');
+ronaldo.push(99);
+ronaldo.push(false);
+console.log(ronaldo);
+var france;
+france = '123';
+france = 123;
+var francais;
+francais = { name: 'messi', age: 35 };
+// Typescript のanyの危険性(型関係なくanyを入れるだけでなんでもはいってしまう)
+var united;
+united = { name: 25, age: 'city' };
+console.log(united);
